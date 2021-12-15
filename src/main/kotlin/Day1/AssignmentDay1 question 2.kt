e0b50ebf4db2b5ebd976cb94cf632b5ddd7f8a98
Koose <KooseTheBlogger@gmail.com>
2021-12-15 19:01:42 +0000

    math linear equation

--- src/main/kotlin/Day1/AssignmentDay1 question 2.kt
+++ src/main/kotlin/Day1/AssignmentDay1 question 2.kt
@@ -1,7 +1,11 @@
 fun main (args: Array<String>){
     var x = 3
     var y = 7 - x
-    var z = x + y -12
+    var z = 12 - x - y
 
     println("find y and z is = y is $y and z is $z")
+    println(x)
+    println(y)
+    println(z)
+    println(x+y+z)
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 }
+
+//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// token
