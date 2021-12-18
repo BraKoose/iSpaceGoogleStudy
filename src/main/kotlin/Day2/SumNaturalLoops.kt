b36850dc2bb3f6ee1ca3a0b46dd1f786a9521c63
Koose <KooseTheBlogger@gmail.com>
2021-12-18 16:41:30 +0000

    Sum natural loops

---
+++ src/main/kotlin/Day2/SumNaturalLoops.kt
@@ -0,0 +1,14 @@
+package Day2
+
+fun main(args: Array<String>) {
+
+    val num = 14
+    var sum = 0
+
+    for (i in 1..num) {
+        // sum = sum+i;
+        sum += i
+    }
+
+    println("Sum = $sum")
+}
